d4557fcbbf5d36f39d6f4633d5114c764ada1fcd
egor <poletaikin.ep@starline.ru>
2023-09-07 21:27:43 +0300

    :art: refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder,
-    middleware::Logger};
 use rustmetric::application::usecases::usecase::UseCase;
 use rustmetric::application::repositories::map::map_storage::Storage;
-use rustmetric::domain::entity::{Metric, MetricKind};
 use tokio;
 
 #[tokio::main]
